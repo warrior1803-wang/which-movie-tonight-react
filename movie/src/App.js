199bfb421d8a26546dd6a5d3dd5a1b3b89aae69b
warrior1803-wang <60429051+warrior1803-wang@users.noreply.github.com>
2022-06-16 09:10:28 +0800

    style design

--- movie/src/App.js
+++ movie/src/App.js
@@ -2,22 +2,33 @@
 
 import React from 'react';
 import MovieSelect from './components/MovieSelect';
-
-import films from './films_data/films.json'
+import background from'./images/background2.jpg'
 function App() {
   
   // const [movie, setmovie] = useState('')
 
   return (
-    <div className="App">
-     
+    <div className="App"
+    style={{
+      width: '100vw',
+      height: '100vh',
+      backgroundImage: `url(${background})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+  
+  
     <MovieSelect></MovieSelect>
-     {films.map((film)=>(
+     {/* {films.map((film)=>(
        <li key={film.link}>
          <h2>{film.movie_title}</h2>
          <h3>{film.link}</h3>
        </li>
-     ))}
+     ))} */}
     </div>
   );
 }
